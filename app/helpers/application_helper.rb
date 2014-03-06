8f74574cdbde70fdce4570ed14522750ab26b23b
rgcidfuentes <rgcidfuentes@users.noreply.github.com>
2014-03-06 11:13:26 +0100

    Change Title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Opticalia VO"
+    base_title = "Opticalia Vila Olímpica"
     if page_title.empty?
       base_title
     else
